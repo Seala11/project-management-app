b306b0ada8e1acc9acd0a9a78830dceadbac5a9c
Valentin Ak <vaakimov@gmail.com>
2022-11-09 00:07:14 +0300

    feat: updates after merge

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,16 @@ import Routing from '../pages/Routing';
 import styles from './app.module.scss';
 import { store } from 'store';
 import { Provider } from 'react-redux';
+import Header from 'components/header/Header';
+import Footer from 'components/header/Header';
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
       <div className={styles.app}>
+        <Header />
         <Routing />
+        <Footer />
       </div>
     </BrowserRouter>
   </Provider>
