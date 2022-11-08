2d80214d80943d7d713559aa8d8f34063c5787e5
Hanna Papova <seal.annuska@gmail.com>
2022-11-08 16:51:29 -0300

    feat: add store

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routing from '../pages/Routing';
 import styles from './app.module.scss';
+import { store } from 'store';
+import { Provider } from 'react-redux';
 
 const App = () => (
-  <BrowserRouter>
-    <div className={styles.app}>
-      <Routing />
-    </div>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <div className={styles.app}>
+        <Routing />
+      </div>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
---
+++ src/store/hooks.ts
@@ -0,0 +1,7 @@
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
+import type { RootState, AppDispatch } from 'store';
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
