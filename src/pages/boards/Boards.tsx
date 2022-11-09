854a8a9d144190c5f4a52f671f22118a2eee343f
Hanna Papova <seal.annuska@gmail.com>
2022-11-09 14:58:51 -0300

    feat: add bords card

--- src/pages/boards/Boards.tsx
+++ src/pages/boards/Boards.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styles from './boards.module.scss';
+import pencil from 'assets/images/pencil.png';
 
 const Boards = () => {
-  return <div>boards</div>;
+  return (
+    <main className={styles.main}>
+      <h2 className={styles.title}>My Boards </h2>
+      <ul className={styles.list}>
+        <li className={`${styles.card} ${styles.cardCreate}`}>
+          <h3 className={styles.cardName}>Create new board</h3>
+          <img src={pencil} alt="yellow pencil" className={styles.image} />
+        </li>
+      </ul>
+    </main>
+  );
 };
 
 export default Boards;
