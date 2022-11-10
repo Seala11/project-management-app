4495b3d9a66d5ca4be6ea808349334f42a9c0566
Valentin Ak <vaakimov@gmail.com>
2022-11-11 00:03:31 +0300

    feat: add boards link, fix styles

--- src/layouts/sectionWelcome/SectionWelcome.tsx
+++ src/layouts/sectionWelcome/SectionWelcome.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import ROUTES from 'utils/constants/ROUTES';
 import styles from './welcome.module.scss';
@@ -6,17 +6,29 @@ import teaching from 'assets/images/teaching.png';
 import { useTranslation } from 'react-i18next';
 
 const SectionWelcome = () => {
+  const [isLogged] = useState(false);
   const { t } = useTranslation();
   return (
     <section className={styles.sectionOne}>
       <div className={styles.wrapper}>
         <div className={styles.buttonsWrapper}>
-          <NavLink to={ROUTES.signIn} title="signIn">
-            <button className={`${styles.btn} ${styles.btnBlue}`}>{t('AUTH.SIGN_IN')}</button>
-          </NavLink>
-          <NavLink to={ROUTES.signUp} title="signUp">
-            <button className={`${styles.btn} ${styles.btnBlue}`}>{t('AUTH.SIGN_UP')}</button>
-          </NavLink>
+          {!isLogged && (
+            <>
+              <NavLink to={ROUTES.signIn} title="signIn">
+                <button className={`${styles.btn} ${styles.btnBlue}`}>{t('AUTH.SIGN_IN')}</button>
+              </NavLink>
+              <NavLink to={ROUTES.signUp} title="signUp">
+                <button className={`${styles.btn} ${styles.btnBlue}`}>{t('AUTH.SIGN_UP')}</button>
+              </NavLink>
+            </>
+          )}
+          {isLogged && (
+            <NavLink to={ROUTES.boards} title="boards">
+              <button className={`${styles.btn} ${styles.btnBlue}`}>
+                {t('WELCOME.BOARDLINK')}
+              </button>
+            </NavLink>
+          )}
         </div>
         <div className={styles.mainContent}>
           <div className={styles.titleWrapper}>
