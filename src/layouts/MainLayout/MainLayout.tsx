6a994bca3024e7ae8dd533735bab2b91fdab818f
Hanna Papova <seal.annuska@gmail.com>
2022-11-12 17:06:37 -0300

    refactor: title mixin and toast constant

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -4,13 +4,14 @@ import Footer from 'components/footer/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import styles from './mainLayout.module.scss';
+import TOASTER from 'utils/constants/TOASTER';
 
 type Props = React.HTMLAttributes<HTMLDivElement>;
 
 const MainLayout = ({ children }: Props) => {
   return (
     <div className={styles.app}>
-      <ToastContainer autoClose={8000} />
+      <ToastContainer autoClose={TOASTER.time} />
       <Header />
       <main className={styles.main}>{children}</main>
       <Footer />
