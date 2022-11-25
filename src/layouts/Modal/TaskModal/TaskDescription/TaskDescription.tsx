2572eb88a5c4dd3dcbaa4e401a791aa30b81fe3c
Hanna Papova <seal.annuska@gmail.com>
2022-11-25 19:18:30 -0300

    feat: add description textarea

--- src/layouts/Modal/TaskModal/TaskDescription/TaskDescription.tsx
+++ src/layouts/Modal/TaskModal/TaskDescription/TaskDescription.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { thunkUpdateTaskInfo } from 'store/middleware/tasks';
 import { toast } from 'react-toastify';
 import Icon from 'components/Icon/Icon';
@@ -17,17 +17,28 @@ const TaskDescription = ({ task, boardId, columnId }: Props) => {
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
 
-  const [descrCurrVal, setDescrCurrVal] = useState(task?.title);
-  const [descrUpdatedVal, setDescrUpdatedVal] = useState(task?.title);
+  const [descrCurrVal, setDescrCurrVal] = useState(task?.description.description);
+  const [descrUpdatedVal, setDescrUpdatedVal] = useState(task?.description.description);
   const [descrInputDisabled, setDescrInputDisabled] = useState(true);
   const descriptionInputRef = useRef<HTMLTextAreaElement | null>(null);
+  const DESCR_MAX_LENGTH = 150;
 
-  const changeTitleValue = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const [cancelPressed, setCancelPressed] = useState(false);
+  const [preventBlur, setPreventBlur] = useState(false);
+
+  const changeDescrValue = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setDescrUpdatedVal(e.target.value);
   };
 
-  const updateTitleVal = () => {
-    if (descrUpdatedVal && task && descrUpdatedVal !== descrCurrVal) {
+  const updateDescriptionVal = () => {
+    if (preventBlur) {
+      setCancelPressed(false);
+      setPreventBlur(false);
+      return;
+    }
+
+    if (descrUpdatedVal && task && descrUpdatedVal !== descrCurrVal && !cancelPressed) {
+      setPreventBlur(true);
       dispatch(
         thunkUpdateTaskInfo({
           _id: task?._id,
@@ -44,18 +55,17 @@ const TaskDescription = ({ task, boardId, columnId }: Props) => {
         })
       )
         .unwrap()
-        .then((originalPromiseResult) => {
-          console.log(originalPromiseResult);
+        .then(() => {
           toast.success('task descr updated');
           setDescrCurrVal(descrUpdatedVal);
         })
-        .catch((rejectedValue) => {
-          console.log(rejectedValue);
+        .catch(() => {
           toast.error('update descr error');
           setDescrUpdatedVal(descrCurrVal);
         })
         .finally(() => {
           setDescrInputDisabled(true);
+          setPreventBlur(false);
         });
     } else {
       setDescrInputDisabled(true);
@@ -63,33 +73,82 @@ const TaskDescription = ({ task, boardId, columnId }: Props) => {
     }
   };
 
-  const updateTitleValByEnter = (e: React.KeyboardEvent) => {
+  const updateDescrValByEnter = (e: React.KeyboardEvent) => {
     if (e.key === 'Enter' && descriptionInputRef && descriptionInputRef.current) {
       descriptionInputRef.current.blur();
     }
   };
 
+  const cancelUpdate = () => {
+    setCancelPressed(true);
+    setPreventBlur(true);
+    setDescrInputDisabled(true);
+    setDescrUpdatedVal(descrCurrVal);
+  };
+
+  useEffect(() => {
+    if (!descrInputDisabled && descriptionInputRef && descriptionInputRef.current) {
+      descriptionInputRef.current.select();
+    }
+  }, [descrInputDisabled]);
+
   return (
     <div className={styles.taskInfo}>
       <div className={styles.changeWrapper}>
         <h3 className={styles.description}>{t('MODAL.DESCRIPTION')}</h3>
-        <button className={styles.changeBtn}>
-          <Icon color="#0047FF" size={16} icon="pen-change" className={styles.icon} />
-        </button>
+        {descrInputDisabled ? (
+          <button className={styles.changeBtn} onClick={() => setDescrInputDisabled(false)}>
+            <Icon color="#0047FF" size={16} icon="pen-change" />
+          </button>
+        ) : (
+          <div>
+            <button className={styles.changeBtn} onMouseDown={updateDescriptionVal}>
+              <Icon color="#0047FF" size={20} icon="done" />
+            </button>
+            <button
+              className={`${styles.changeBtn} ${styles.changeBtnCancel}`}
+              onMouseDown={cancelUpdate}
+            >
+              <Icon color="#cc0707" size={20} icon="cancel" />
+            </button>
+          </div>
+        )}
       </div>
 
-      <textarea
-        maxLength={150}
-        ref={descriptionInputRef}
-        className={`${styles.textarea} ${descrInputDisabled ? styles.disabled : styles.abled}`}
-        value={descrInputDisabled ? descrCurrVal : descrUpdatedVal}
-        onClick={() => setDescrInputDisabled(false)}
-        onBlur={updateTitleVal}
-        onChange={(e) => changeTitleValue(e)}
-        onKeyDown={updateTitleValByEnter}
-      >
-        {task?.description.description}
-      </textarea>
+      <div className={styles.textareaWrapper}>
+        {descrInputDisabled ? (
+          <p
+            onClick={() => {
+              setDescrInputDisabled(false);
+            }}
+            className={styles.textDescription}
+          >
+            {descrCurrVal}
+          </p>
+        ) : (
+          <textarea
+            maxLength={150}
+            ref={descriptionInputRef}
+            className={styles.textarea}
+            value={descrUpdatedVal}
+            onBlur={updateDescriptionVal}
+            onChange={(e) => changeDescrValue(e)}
+            onKeyDown={updateDescrValByEnter}
+          />
+        )}
+
+        {!descrInputDisabled && (
+          <div
+            className={`${styles.counter} ${
+              descrUpdatedVal && descrUpdatedVal.length > 140 ? styles.counterLim : ''
+            }`}
+          >
+            <span>{(descrUpdatedVal && descrUpdatedVal.length) || 0}</span>
+            <span>/</span>
+            <span>{DESCR_MAX_LENGTH}</span>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
