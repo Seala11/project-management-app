8a14c3ef25a70adf357f0ac828e77bb9182039a4
Pavlov Kostyantyn <realanka77@gmail.com>
2022-11-29 09:51:59 +0200

    refactor: change base url and turn off strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import './setupi18n';
 const root = ReactDOM.createRoot(document.getElementById('root') ?? document.body);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
