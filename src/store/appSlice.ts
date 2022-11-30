235bb704fde5f8ce12b44c150b121ce7805541b3
Kostiantyn Pavlov <realanka77@gmail.com>
2022-11-30 18:44:03 +0200

    refactor: delete unused translate

--- src/store/appSlice.ts
+++ src/store/appSlice.ts
@@ -1,20 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
-import type { PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from 'store';
 
-export enum Lang {
-  RU = 'ru',
-  EN = 'en',
-}
-
 type AppState = {
-  lang: Lang;
   toastMessage: string | null;
   isPending: boolean;
 };
 
 export const initialState: AppState = {
-  lang: Lang.EN,
   toastMessage: null,
   isPending: true,
 };
@@ -23,10 +15,6 @@ export const appSlice = createSlice({
   name: 'app',
   initialState,
   reducers: {
-    setLang: (state, action: PayloadAction<Lang>) => {
-      state.lang = action.payload;
-    },
-
     setToastMessage: (state, action) => {
       state.toastMessage = action.payload;
     },
@@ -37,9 +25,7 @@ export const appSlice = createSlice({
   },
 });
 
-export const { setLang, setToastMessage, setIsPending } = appSlice.actions;
-
-export const languageSelector = (state: RootState) => state.app.lang;
+export const { setToastMessage, setIsPending } = appSlice.actions;
 export const toastMessageSelector = (state: RootState) => state.app.toastMessage;
 export const appSelector = (state: RootState) => state.app;
 
