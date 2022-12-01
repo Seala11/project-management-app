985ec66cfe7dcbbcae7eecbbb8693187d3cdad9a
Kostiantyn Pavlov <realanka77@gmail.com>
2022-12-01 21:09:40 +0200

    fix: double toast

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,12 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Header from 'components/header/Header';
 import Footer from 'components/footer/Footer';
-import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { getTokenFromLS } from 'utils/func/localStorage';
-import { thunkGetUserById } from 'store/authSlice';
-import { parseJwt } from 'utils/func/parsejwt';
-import { useTranslation } from 'react-i18next';
 import {
   modalStatusSelector,
   setModalClose,
@@ -20,10 +15,8 @@ import Modal from 'layouts/Modal/Modal';
 import ConfirmationModal from 'layouts/Modal/ConfirmationModal/ConfirmationModal';
 import TaskModal from 'layouts/Modal/TaskModal/TaskModal';
 import Loader from 'components/loader/Loader';
-import { appSelector, setIsPending } from 'store/appSlice';
+import { appSelector } from 'store/appSlice';
 import { singleBoardRequestStatus } from 'store/boardSlice';
-import { getErrorMessage } from 'utils/func/handleError';
-import { getMsgError } from 'utils/func/getMsgError';
 
 type Props = React.HTMLAttributes<HTMLDivElement>;
 
@@ -34,26 +27,7 @@ const MainLayout = ({ children }: Props) => {
   const { isPending } = useAppSelector(appSelector);
   const pending = boardState || isPending;
   const dispatch = useAppDispatch();
-  const { t } = useTranslation();
   const taskDeleteConfirmMessage = useAppSelector(taskDeleteConfirmSelector);
-
-  useEffect(() => {
-    if (getTokenFromLS()) {
-      const token = getTokenFromLS();
-      dispatch(thunkGetUserById({ userId: parseJwt(token).id, token: token }))
-        .unwrap()
-        .then()
-        .catch((err) => {
-          const error = getErrorMessage(err);
-          toast.error(t(getMsgError(error)));
-        })
-        .finally(() => dispatch(setIsPending(false)));
-    } else {
-      dispatch(setIsPending(false));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   const closeModal = (event: React.MouseEvent) => {
     if (event.target === event.currentTarget) {
       dispatch(setModalClose());
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SectionFeatures from 'layouts/sectionFeatures/SectionFeatures';
 import SectionTeam from 'layouts/sectionTeam/SectionTeam';
 import SectionWelcome from 'layouts/sectionWelcome/SectionWelcome';
+import { getTokenFromLS } from 'utils/func/localStorage';
+import { thunkGetUserById } from 'store/authSlice';
+import { parseJwt } from 'utils/func/parsejwt';
+import { getErrorMessage } from 'utils/func/handleError';
+import { getMsgError } from 'utils/func/getMsgError';
+import { setIsPending } from 'store/appSlice';
+import { toast } from 'react-toastify';
+import { useTranslation } from 'react-i18next';
+import { useAppDispatch } from 'store/hooks';
 
 const Home = () => {
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (getTokenFromLS()) {
+      console.log('1');
+
+      const token = getTokenFromLS();
+      dispatch(thunkGetUserById({ userId: parseJwt(token).id, token: token }))
+        .unwrap()
+        .then()
+        .catch((err) => {
+          const error = getErrorMessage(err);
+          toast.error(t(getMsgError(error)));
+        })
+        .finally(() => dispatch(setIsPending(false)));
+    } else {
+      dispatch(setIsPending(false));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
       <SectionWelcome />
